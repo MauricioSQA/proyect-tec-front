a154491b95e1adf99048863b2e94fb0b4716eafb
MauricioSQA <114277830+MauricioSQA@users.noreply.github.com>
2024-07-17 01:10:38 -0500

    Update contacto.component.ts

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -20,33 +20,22 @@ constructor(){}
 
 onInput(event: any) {
 
-  const value = event.target.value; // Obtener el valor del campo de entrada
-  const name = event.target.name;   // Obtener el nombre del campo de entrada
-
-  // Dependiendo del nombre del campo de entrada, puedes realizar diferentes acciones
+  const value = event.target.value; 
+  const name = event.target.name;   
   switch (name) {
     case 'nombre':
       this.firstName = event.target.value;
-      console.log("Valor del campo 'nombre':", value);
-      // Realiza acciones específicas para el campo 'nombre'
       break;
     case 'correo':
       this.correo=event.target.value;
-      console.log("Valor del campo 'correo':", value);
-      // Realiza acciones específicas para el campo 'correo'
       break;
     case 'asunto':
     this.asunto=event.target.value;
-    console.log("Valor del campo 'correo':", value);
-    // Realiza acciones específicas para el campo 'correo'
     break;
     case 'mensaje':
     this.mensaje=event.target.value;
-    console.log("Valor del campo 'correo':", value);
-    // Realiza acciones específicas para el campo 'correo'
       break;
     default:
-      // Maneja otros casos si es necesario
       break;
   }
 }
@@ -58,12 +47,10 @@ preview(e:Event) {
     alert("TODOS LOS CAMPOS SON OBLIGATORIOS");
   }
   const form={
-
                 nombre:this.firstName,
                 correo:this.correo,
                 tema:this.asunto,
                 mensaje:this.mensaje
-
               }
 
    fetch("https://formsubmit.co/ajax/contactodesperdiciocero@gmail.com", {
@@ -77,11 +64,8 @@ preview(e:Event) {
     body: JSON.stringify( form )
 
     })
-    .then(response => { console.log("HOLA ......."); return response.json();})
+    .then(response => {return response.json();})
     .then(data => {
-
-            console.log(data);
-
     //SI EL EMAIL FUE ENVIADO CORRECTAMENTE
     if(data.success=="true"){
       Swal.fire({
@@ -90,6 +74,6 @@ preview(e:Event) {
       });
     }
     })
-    .catch(error => console.log(error)); // EN CASO HAY ALGÚN ERROR AL REALIZAR EL ENVÍO DEL FORMULARIO
+    .catch(error => (error));
  }
 }
